a2362ca2eb14334475c887716ae6be5a6d61773b
Informatica <informatica@alumno.local>
2024-09-05 17:21:45 -0600

    navegacion adelantada

--- src/service/characters.jsx
+++ src/service/characters.jsx
@@ -1,14 +1,15 @@
 import CryptoJS from "crypto-js"
 
 const time_stamp = new Date().getTime()
-const private_api_key = '80c1c100a09102f383dd2a0e0c489d401708cf69'
-const public_api_key = 'd0ff978e5afae35b61afcc60ee2ddcaa'
+const private_api_key = '8aee549f26d27146aa1a997383384e15b829817a'
+const public_api_key = '5e4dc7240ff9294c45f87641a908954c'
 
 const hash = CryptoJS.MD5(time_stamp + private_api_key + public_api_key).toString();
 
-export const reqCharacters = async () => {
-    const resp = await fetch(`https://gateway.marvel.com:443/v1/public/characters?ts=${time_stamp}&apikey=${public_api_key}&hash=${hash}`)
+export const reqCharacter = async (page = 1, limit = 20) => {
+    const offset = (page - 1) * limit
+    const resp = await fetch(`https://gateway.marvel.com:443/v1/public/characters?ts=${time_stamp}&apikey=${public_api_key}&hash=${hash}&offset=${offset}`)
     const { data } = await resp.json()
-
+    console.log(data)
     return data
 }
